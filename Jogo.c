62c5c4771c88189d1266f6d310547c498bd1866a
skvsr <samukvsr@gmail.com>
2025-03-23 22:21:14 -0300

    git commit -m "Inicio do projeto"

--- Jogo.c
+++ Jogo.c
@@ -1,4 +1,27 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <string.h>
+
+// struct do personagem
+typedef struct {
+    char nome[50];
+    int forca;
+    int velocidade;
+} Personagem;
+
+// struct do inimigo
+typedef struct {
+    char nome[50];
+    int forca;
+    int velocidade;
+} Inimigo;
+
+// struct de item
+typedef struct {
+    char nome[50];
+    int valor;
+} Item;
 
 int main() {
     int hj;
